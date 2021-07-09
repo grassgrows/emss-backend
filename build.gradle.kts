5e112ac56eda4fb4e5fff8a7be99ffbcb7e39af3
takanashi <1303170174@qq.com>
2021-07-09 08:48:54 +0800

    :bug: 修复依赖bug

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,7 +40,7 @@ dependencies {
     implementation("org.ktorm:ktorm-core:$ktorm_version")
     // Testing
     testImplementation("io.ktor:ktor-server-test-host:1.6.1")
-    testImplementation("io.insert-koin:ktor-server-tests:$ktor_version")
+    testImplementation("io.ktor:ktor-server-tests:$ktor_version")
     testImplementation("io.insert-koin:koin-test:$koin_version")
     testImplementation("junit:junit:4.12")
 }
