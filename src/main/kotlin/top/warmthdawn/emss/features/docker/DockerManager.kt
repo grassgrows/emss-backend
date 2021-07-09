103c16983d290d95d81e041007547f1ca08d4fcf
takanashi <1303170174@qq.com>
2021-07-09 17:31:38 +0800

    :whale: Docker重构拉取镜像功能
    进行中

--- src/main/kotlin/top/warmthdawn/emss/features/docker/DockerManager.kt
+++ src/main/kotlin/top/warmthdawn/emss/features/docker/DockerManager.kt
@@ -1,23 +1,20 @@
 package top.warmthdawn.emss.features.docker
 
 import com.github.dockerjava.api.DockerClient
-import com.github.dockerjava.api.async.ResultCallback
+import com.github.dockerjava.api.command.PullImageResultCallback
 import com.github.dockerjava.api.model.*
 import com.github.dockerjava.core.DefaultDockerClientConfig
 import com.github.dockerjava.core.DockerClientImpl
 import com.github.dockerjava.httpclient5.ApacheDockerHttpClient
-import kotlinx.coroutines.suspendCancellableCoroutine
-import java.io.File
+import org.slf4j.LoggerFactory
+import java.io.Closeable
 import java.time.Duration
-import kotlin.coroutines.resume
+import javax.swing.text.html.HTML
 
-class DockerManager(
-    //registryUser: String,
-    //registryPass: String,
-    //registryMail: String,
-    registryUrl: String
-) {
+object DockerManager {
+    val registryUrl = "https://index.docker.io/v1/"
     private val dockerClient: DockerClient
+    private val log = LoggerFactory.getLogger(DockerManager::class.java)
 
     // 初始化并连接Docker
     init {
@@ -27,18 +24,13 @@ class DockerManager(
             "unix:///var/run/docker.sock"
         val clientConfig = DefaultDockerClientConfig.createDefaultConfigBuilder()
             .withDockerHost(dockerHost)
-//            .withDockerTlsVerify(docker_tls_verity)
-//            .withDockerCertPath(docker_cert_path)
-            //.withRegistryUsername(registryUser)
-            //.withRegistryPassword(registryPass)
-            //.withRegistryEmail(registryMail)
             .withRegistryUrl(registryUrl)
             .build()
 
         val httpClient = ApacheDockerHttpClient.Builder()
-            .dockerHost(clientConfig.getDockerHost())
-            .sslConfig(clientConfig.getSSLConfig())
-            .maxConnections(100)
+            .dockerHost(clientConfig.dockerHost)
+            .sslConfig(clientConfig.sslConfig)
+            .maxConnections(1)
             .connectionTimeout(Duration.ofSeconds(30))
             .responseTimeout(Duration.ofSeconds(45))
             .build()
@@ -46,17 +38,69 @@ class DockerManager(
         dockerClient = DockerClientImpl.getInstance(clientConfig, httpClient)
     }
 
-    // 拉取镜像
-    suspend fun pullImage(imageName: String)
-    {
-        suspendCancellableCoroutine<Boolean> {
-            val callback = dockerClient
-                .pullImageCmd(imageName)
-                .start()
+    private data class DownloadingStatus(
+        var current: Long,
+        var total: Long
+    )
 
-            callback.awaitCompletion()
-            it.resume(true)
-        }
+    fun pullImage(repository: String, tag: String? = null): PullImageResultCallback {
+
+        val imageName = repository + if (!tag.isNullOrEmpty()) ":$tag" else ":latest"
+        val status = mutableMapOf<String, DownloadingStatus>()
+        var time: Long? = null
+        var schedule: Double
+        var lastCurrent: Long = 0
+
+
+        val callback = dockerClient
+            .pullImageCmd(imageName)
+            .exec(object : PullImageResultCallback() {
+                override fun onStart(closeable: Closeable) {
+                    super.onStart(closeable)
+                    log.info("开始下载镜像：$imageName")
+                }
+
+                override fun onNext(pullResponseItem: PullResponseItem) {
+                    super.onNext(pullResponseItem)
+                    when (pullResponseItem.status!!) {
+                        "Pulling fs layer" -> status[pullResponseItem.id!!] = DownloadingStatus(0, 1)
+                        "Verifying Checksum", "Download complete" -> status.remove(pullResponseItem.status)
+                        "Downloading" -> {
+                            status[pullResponseItem.id]!!.current = pullResponseItem.progressDetail!!.current!!
+                            status[pullResponseItem.id]!!.total = pullResponseItem.progressDetail!!.total!!
+                            var allTotal = 0.00
+                            var allCurrent = 0L
+                            status.values.forEach {
+                                allTotal += it.total
+                                allCurrent += it.current
+                            }
+                            schedule = allCurrent / allTotal
+
+                            if (time != null) {
+                                val thisTime = System.currentTimeMillis()
+                                val speed = (allCurrent - lastCurrent) / (thisTime - time!!)
+                                lastCurrent = allCurrent
+                                log.info("下载进度：${schedule * 100}%, 下载速度：${speed * 1000.0 / (1024 * 1024 * 8)}MB/S")
+                            }
+                            time = System.currentTimeMillis()
+                        }
+                    }
+                }
+
+                override fun onError(throwable: Throwable) {
+                    super.onError(throwable)
+                    log.error("镜像下载失败：$imageName", throwable.message)
+
+                }
+
+                override fun onComplete() {
+                    super.onComplete()
+                    log.info("镜像下载完成：$imageName")
+                }
+            })
+
+        return callback;
+
     }
 
     // 创建容器(需添加启动命令)
@@ -96,36 +140,5 @@ class DockerManager(
             .stopContainerCmd(containerName)
             .exec()
     }
-
-    // 重启容器
-    fun restartContainer(containerName: String) {
-        dockerClient
-            .restartContainerCmd(containerName)
-            .exec()
-    }
-
-
-    //region 废弃功能
-    /*
-    fun createDockerfile(path: String, text: String) {
-        val file = File(path)
-        file.writeText(text)
-    }
-
-    // 根据Dockerfile构建镜像
-    fun buildImage(dockerfile: String, imageName: String): String {
-
-
-        val imageid = dockerClient.buildImageCmd()
-            .withDockerfilePath(dockerfile)
-            .withTags(setOf(imageName))
-            .start()
-            .awaitImageId()
-
-
-        return imageid
-    }
-
-     */
-    //endregion
 }
+
--- src/test/kotlin/top/warmthdawn/emss/features/docker/DockerManagerTest.kt
+++ src/test/kotlin/top/warmthdawn/emss/features/docker/DockerManagerTest.kt
@@ -2,39 +2,27 @@ package top.warmthdawn.emss.features.docker
 
 import com.github.dockerjava.api.model.Bind
 import com.github.dockerjava.api.model.Volume
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.awaitAll
-import kotlinx.coroutines.launch
 import org.junit.Test
-import kotlin.coroutines.resume
 
 
 internal class DockerManagerTest {
     @Test
     fun pullImageTest() {
-        val dockerManager = DockerManager(
-            /*registryUser = "takanashirikka1412", registryPass = "docg20092010ker",
-            registryMail = "1303170174@qq.com",*/ registryUrl = "https://index.docker.io/v1/"
-        )
         val imageName = "java"
-        val callback = GlobalScope.launch {
-            dockerManager.pullImage(imageName)
-            print("downloading...")
-        }
+        print("downloading...")
+        val callback = DockerManager.pullImage(imageName)
+        callback.awaitCompletion()
+
     }
 
     @Test
     fun createContainerTest() {
-        val dockerManager = DockerManager(
-            /*registryUser = "takanashirikka1412", registryPass = "docg20092010ker",
-            registryMail = "1303170174@qq.com",*/ registryUrl = "https://index.docker.io/v1/"
-        )
         val containerName = "testContainer005"
         val bind = Bind("/data/$containerName", Volume("/data"))
         val cmd = listOf("/bin/sh", "-c", "while true; do echo hello world; sleep 1; done")
         //if()
         print(
-            dockerManager.createContainer(
+            DockerManager.createContainer(
                 containerName, "ubuntu",
                 "127.0.0.1"/*"172.28.89.242"*/, 8083, 8084, bind, cmd
             )
@@ -43,32 +31,14 @@ internal class DockerManagerTest {
 
     @Test
     fun startContainerTest() {
-        val dockerManager = DockerManager(
-            /*registryUser = "takanashirikka1412", registryPass = "docg20092010ker",
-            registryMail = "1303170174@qq.com",*/ registryUrl = "https://index.docker.io/v1/"
-        )
         val containerName = "testContainer005"
-        dockerManager.startContainer(containerName)
+        DockerManager.startContainer(containerName)
     }
 
     @Test
     fun stopContainerTest() {
-        val dockerManager = DockerManager(
-            /*registryUser = "takanashirikka1412", registryPass = "docg20092010ker",
-            registryMail = "1303170174@qq.com",*/ registryUrl = "https://index.docker.io/v1/"
-        )
         val containerName = "testContainer005"
-        dockerManager.stopContainer(containerName)
-    }
-
-    @Test
-    fun restartContainerTest() {
-        val dockerManager = DockerManager(
-            /*registryUser = "takanashirikka1412", registryPass = "docg20092010ker",
-            registryMail = "1303170174@qq.com",*/ registryUrl = "https://index.docker.io/v1/"
-        )
-        val containerName = "testContainer005"
-        dockerManager.restartContainer(containerName)
+        DockerManager.stopContainer(containerName)
     }
 
 
