a91eed306a709c8fafd507fc98eaddf898a012c0
Doctor_Yin <1921841421@qq.com>
2021-07-29 20:50:41 +0800

    :sparkles: 加入服务器tps监听

--- src/main/kotlin/top/warmthdawn/emss/features/statistics/minecraft/ping/MinecraftTools.kt
+++ src/main/kotlin/top/warmthdawn/emss/features/statistics/minecraft/ping/MinecraftTools.kt
@@ -1,6 +1,8 @@
 package top.warmthdawn.emss.features.statistics.minecraft.ping
 
 import top.limbang.doctor.client.MinecraftClient
+import top.limbang.doctor.client.running.TpsEntity
+import top.limbang.doctor.client.running.tpsTools
 import java.util.concurrent.TimeUnit
 
 /**
@@ -8,10 +10,21 @@ import java.util.concurrent.TimeUnit
  * @author WarmthDawn
  * @since 2021-07-21
  */
-object MinecraftTools{
+object MinecraftTools {
     val timeout = 5 * 1000L
     fun ping(port: Int) = kotlin.runCatching {
-        MinecraftClient.ping("localhost", port, timeout,  TimeUnit.SECONDS)
+        MinecraftClient.ping("localhost", port, timeout, TimeUnit.SECONDS)
+    }.getOrNull()
+
+    fun tps(client: MinecraftClient) = kotlin.runCatching {
+        val tpsEntityList = client.tpsTools.getTps().get()
+        var tpsEntity: TpsEntity? = null
+        for (i in tpsEntityList.indices) {
+            if (tpsEntityList[i].dim == "Overall") {
+                tpsEntity = tpsEntityList[i]
+            }
+        }
+        tpsEntity?.tps ?: 20.0
     }.getOrNull()
 }
 
